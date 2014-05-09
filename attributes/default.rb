98b6bcccc229d92483f44b042cd9d9a197f212cd
Jason Boyles <jason@duncancreek.net>
2014-05-09 16:55:51 -0500

    add innodb_buffer_pool_mempercent attrib default

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,2 @@
-default[:rax_mysql_tunables][:config_options][:mysqld]['innodb-buffer-pool-size'] = "#{(node['memory']['total'].to_i * 0.6 ).floor * 1024}"
+default[:rax_mysql_tunables][:innodb_buffer_pool_mempercent] = 0.6
+default[:rax_mysql_tunables][:config_options][:mysqld]['innodb-buffer-pool-size'] = "#{(node['memory']['total'].to_i * node[:rax_mysql_tunables][:innodb_buffer_pool_mempercent].to_f ).floor * 1024}"
